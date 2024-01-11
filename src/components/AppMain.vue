d2023cea3c1f14b4437772b4ae6e30bc79faf6be
igorgaidolfi <gandalf.4@outlook.it>
2024-01-11 15:35:38 +0100

    fixed singlecards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,12 @@
 <script>
+import singleCards from './singleCards.vue';
 import { store } from '../store.js'
 
 export default {
     name: 'AppMain',
+    components: {
+        singleCards,
+    },
     data() {
         return {
             store,
@@ -19,13 +23,7 @@ export default {
         </div>
         <div class="container bg-white">
             <div class="row">
-                <div class="col-20" v-for="card,index in store.cardsList" :key="index">
-                    <div class="cards_container">
-                    <img :src="card.card_images[0].image_url" :alt="card.name">
-                    <h5 class="text-center my-3">{{card.name}}</h5>
-                    <div class="text-center archetype">{{card.archetype}}</div>
-                    </div>
-                </div>
+                <singleCards v-for="card,index in store.cardsList" :key="index" :card="card"/>
             </div>
         </div>
   </main>
@@ -33,41 +31,7 @@ export default {
 <style lang="scss">
 @use '../styles/partials/variables' as*;
 
-main {
-    background-color: $orange;
-}
-
 .bg-white {
     background-color: $white;
-}
-
-.col-20 {
-    width: calc(100%/5 - 20px);
-    margin: 10px;
-
-    .cards_container {
-        background-color: $orange;
-        width: 100%;
-        margin-bottom: 15px;
-        height: calc(100% - 15px);
-
-        img {
-            max-width: 100%;
-        }
-
-        h5 {
-            color: $white;
-            text-transform: uppercase;
-            font-weight: 600;
-            text-align: center;
-            margin: 1rem 0;
-        }
-
-        .archetype {
-            font-size: 20px;
-            text-align: center;
-        }
-
-    }
 }
 </style>
--- src/components/singleCards.vue
+++ src/components/singleCards.vue
@@ -7,12 +7,44 @@ export default {
 }
 </script>
 <template lang="">
-<div class="col-3">
-    <img :src="card.cardsList[0].card_images[0].image_url" :alt="card.cardsList[0].name">
-    <h5>{{card.cardsList[0].name}}</h5>
-    <div>{{card.cardsList[0].archetype}}</div>
+<div class="col-20">
+    <div class="cards_container">
+    <img :src="card.card_images[0].image_url" :alt="card.name">
+    <h5 class="text-center my-3">{{card.name}}</h5>
+    <div class="text-center archetype">{{card.archetype}}</div>
+    </div>
 </div>
 </template>
-<style lang="">
-    
+<style lang="scss">
+@use '../styles/partials/variables' as*;
+
+.col-20 {
+    width: calc(100%/5 - 20px);
+    margin: 10px;
+
+    .cards_container {
+        background-color: $orange;
+        width: 100%;
+        margin-bottom: 15px;
+        height: calc(100% - 15px);
+
+        img {
+            max-width: 100%;
+        }
+
+        h5 {
+            color: $white;
+            text-transform: uppercase;
+            font-weight: 600;
+            text-align: center;
+            margin: 1rem 0;
+        }
+
+        .archetype {
+            font-size: 20px;
+            text-align: center;
+        }
+
+    }
+}
 </style>
